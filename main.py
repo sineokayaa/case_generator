f3a9554ea59015f2375c3fd16c478b5b0b0672fe
sineokayaa <a.sineokaya@g.nsu.ru>
2023-11-11 21:08:50 +0700

    final

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 '''
-
-
+Group:
+Varfolomeeva Viktoria
+Sineokaya Anastasiya 55
 '''
 
 from random import randint
@@ -17,21 +18,22 @@ with open('input.txt', encoding='utf8') as f_in:
             for i in range(len(ptr)):
                 if ptr[i] not in words_uniq:
                     words_uniq.append(ptr[i])
-                if ptr[i] not in words_up and ptr[i][0] in 'ЙЦУКЕНГШЩЗХЪЭЖДЛОРПАВЫФЯЧСМИТЬБЮQWERTYUIOPLKJHGFDSAZXCVBNM':
+                letters = 'ЙЦУКЕНГШЩЗХЪЭЖДЛОРПАВЫФЯЧСМИТЬБЮQWERTYUIOPLKJHGFDSAZXCVBNM'
+                if ptr[i] not in words_up and ptr[i][0] in letters:
                     words_up.append(ptr[i])
                 words.append(ptr[i])
-print(words)
-print(words_uniq)
-print(words_up)
+
 words_for_words = []
+
 for i in range(len(words_uniq)):
     ind = words.index(words_uniq[i])
     wrd = words[ind:]
     while words_uniq[i] in wrd:
         wrd.remove(words_uniq[i])
     words_for_words.append(wrd)
-print(words_for_words)
+
 count_ptrs = 0
+
 with open('output.txt', 'w', encoding='utf8') as f_out:
     while count_ptrs != ptrs:
         ind_1 = randint(0, len(words_up) - 1)
@@ -39,11 +41,16 @@ with open('output.txt', 'w', encoding='utf8') as f_out:
         word = word_1
         sentence = word_1
         count_wrds = 0
-        while word[-1] not in '.?!' or count_wrds > 20:
+
+        while word[-1] not in '.?!' and count_wrds <= 20:
             ind = words_uniq.index(word)
+            while not words_for_words[ind]:
+                ind = randint(0, len(words_uniq) - 1)
+
             ind_new = randint(0, len(words_for_words[ind]) - 1)
             word = words_for_words[ind][ind_new]
-            sentence += word
+            sentence += ' ' + word
             count_wrds += 1
-        count_ptrs += 1
 
+        print(sentence, file=f_out)
+        count_ptrs += 1
