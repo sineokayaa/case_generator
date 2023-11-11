fa4ac1147e18109ebabd64ef941f52a4a4a450f7
Varfolomeevaa <v.varfolomeeva@g.nsu.ru>
2023-11-11 19:02:26 +0700

    123

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-fdfgd
+from random import randint
+
+words_uniq = []
+words = ''
+
+with open('input.txt') as f_in:
+    ptrs = f_in.readline()
+    for x in f_in:
+        if x != ptrs:
+            ptr = x.split()
+            for i in range(len(ptr)):
+                if ptr[i] not in words:
+                    words_uniq.append(ptr[i])
+                words += ptr[i]
+
+words_for_words = []
+for i in range(len(words_uniq)):
+    ind = words.rfind(words_uniq[i])
+    wrd = words[:ind].split()
+    while words_uniq[i] in wrd:
+        wrd.remove(words_uniq[i])
+    words_for_words.append(wrd)
